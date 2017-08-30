a4af1468537b5ce168df8ab98c75f7be7a3bca5c
Filip Stanek <fstanek@gmail.com>
2017-08-30 18:18:23 -0400

    Revert "Revert "Initial Checkin""
    This reverts commit 5007484ac09986074f30a962dcb47338091fcfa4.

---
+++ src/Controllers/Api/Public/CategoriesController.cs
@@ -0,0 +1,44 @@
+﻿using Blogifier.Core.Controllers;
+using Blogifier.Core.Data.Domain;
+using Blogifier.Core.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApp.Controllers.Api.Public
+{
+    [Route("blogifier/api/public/[controller]")]
+    public class CategoriesController : Controller
+    {
+        IUnitOfWork _db;
+        private readonly ILogger _logger;
+
+        public CategoriesController(IUnitOfWork db, ILogger<BlogController> logger)
+        {
+            _db = db;
+            _logger = logger;
+        }
+
+        // GET: blogifier/api/public/categories
+        /// <summary>
+        /// Get all categories
+        /// </summary>
+        public IEnumerable<Category> Get()
+        {
+            return _db.Categories.Find(c => c.LastUpdated >= DateTime.MinValue);            
+        }
+
+        // GET: blogifier/api/public/categories/filip-stanek
+        /// <summary>
+        /// Get all categories for author
+        /// </summary>
+        [Route("{author}")]
+        public IEnumerable<Category> Get(string author)
+        {
+            Profile profile = _db.Profiles.Single(p => p.Slug == author);
+            return _db.Categories.Find(c => c.ProfileId == profile.Id);
+        }
+    }
+}
